c10554795d08a8c9a7ef7100f6468d98ca217f28
iavzfer782 <contactoismaelvazquez@gmail.com>
2026-03-29 15:04:52 +0200

    Funcion añadida de Recuento Informacion

--- funciones.py
+++ funciones.py
@@ -1,19 +1,32 @@
 import json
-def cargar_fichero(ruta="ejercicios.json"):
-    with open(ruta, encoding="utf-8") as f:
+def cargar_fichero(ruta='ejercicios.json'):
+    with open(ruta, encoding='utf-8') as f:
         datos = json.load(f)
-    return datos["ejercicios"]
+    return datos['ejercicios']
 
 def listar_ejercicios(ejercicios):
         for ejercicio in ejercicios:
-            titulo = ejercicio["nombre"]["visible"]
-            tipo = ejercicio["clasificacion"]["tipo"]["nombre"]
-            musculo = ejercicio["musculos"]["principal"]["nombre"]
+            titulo = ejercicio['nombre']['visible']
+            tipo = ejercicio['clasificacion']['tipo']['nombre']
+            musculo = ejercicio['musculos']['principal']['nombre']
             
-            print("Titulo: ", titulo)
-            print("Tipo: ", tipo)
-            print("Musculo Principal", musculo)
-            print("------------------")
+            print('Título: ', titulo)
+            print('Tipo: ', tipo)
+            print('Musculo Principal', musculo)
+            print('------------------')
             print()
-            
-            
+
+def recuento_informacion(ejercicios):
+    total_ejercicios = len(ejercicios)
+    
+    for ejercicio in ejercicios:
+        titulo = ejercicio['nombre']['visible']
+        total_pasos = len(ejercicio['ejecucion']['pasos'])
+        
+        print('Título: ', titulo)
+        print('Número de pasos: ',total_pasos)
+        print('------------------')
+        print()
+        
+    print('Hay un total de ', total_ejercicios, ' ejercicios.')
+    
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from funciones import *
-ejercicios = cargar_fichero("ejercicios.json")
+ejercicios = cargar_fichero('ejercicios.json')
 
 salir = False
 
 while salir == False:
-    print("\n--- MENÚ EJERCICIOS ---")
-    print("1. Listado Ejercicios")
-    print("6. Salir")
+    print('\n--- MENÚ EJERCICIOS ---')
+    print('1. Listado Ejercicios')
+    print('2. Recuento Información')
+    print('6. Salir')
     
-    opcion = input("Elige una opción: ")
+    opcion = input('Elige una opción: ')
   
-    if opcion == "1":
+    if opcion == '1':
         listar_ejercicios(ejercicios)
-    elif opcion == "5":
-        print("Saliendo...")
+    elif opcion == '2':
+        recuento_informacion(ejercicios)
+    elif opcion == '6':
+        print('Saliendo...')
         salir = True
     else:
-        print("Opción no válida, elige entre 1 y 6.")
+        print('Opción no válida, elige entre 1 y 6.')
     
